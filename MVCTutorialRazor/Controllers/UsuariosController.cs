84b4bdfe58637b922cf273ed463756325ad06418
pablolopaltair <csi22-plopjan@colegioaltair.net>
2022-11-14 11:40:35 +0100

    Agregar archivos de proyecto.

---
+++ MVCTutorialRazor/Controllers/UsuariosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MVCTutorialRazor.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListaUsuarios()
+        {
+            List<string> listaUsuarios = new List<string>();
+            listaUsuarios.Add("Pablo López");
+            listaUsuarios.Add("Jesús Patricio");
+            listaUsuarios.Add("Javi Mena");
+            listaUsuarios.Add("Juan Carlos Bada");
+            listaUsuarios.Add("Javi Cano");
+            listaUsuarios.Add("Alberto Talamino");
+            return View(listaUsuarios);
+        }
+    }
+}
